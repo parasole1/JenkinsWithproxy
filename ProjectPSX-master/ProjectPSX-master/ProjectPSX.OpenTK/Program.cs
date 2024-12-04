6069d10a922a47b0ae9a96dd64722563af4c6145
parasole1 <93145059+parasole1@users.noreply.github.com>
2024-12-04 10:36:10 +0545

    Add files via upload

---
+++ ProjectPSX-master/ProjectPSX-master/ProjectPSX.OpenTK/Program.cs
@@ -0,0 +1,26 @@
+using OpenTK.Mathematics;
+using OpenTK.Windowing.Desktop;
+using OpenTK.Windowing.Common;
+using System;
+
+namespace ProjectPSX.OpenTK {
+    static class Program {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main() {
+            GameWindowSettings settings = new GameWindowSettings();
+            settings.UpdateFrequency = 60;
+            NativeWindowSettings nativeWindow = new NativeWindowSettings();
+            nativeWindow.API = ContextAPI.OpenGL;
+            nativeWindow.Size = new Vector2i(1024, 512);
+            nativeWindow.Title = "ProjectPSX";
+            nativeWindow.Profile = ContextProfile.Compatability;
+
+            Window window = new Window(settings, nativeWindow);
+            window.VSync = VSyncMode.On;
+            window.Run();
+        }
+    }
+}
